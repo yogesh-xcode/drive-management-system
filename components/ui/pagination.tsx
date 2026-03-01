6a9a25ae489fd80d51ec3cd29446bff62402e83a
Yogesh <yogesh.xcode@gmail.com>
2026-03-01 19:47:24 +0530

    refactor(ui): unify icons and refresh core UI primitives

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  MoreHorizontalIcon,
-} from "lucide-react";
+  IconChevronLeft,
+  IconChevronRight,
+  IconDots,
+} from "@/lib/icons";
 
 import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "@/components/ui/button";
@@ -76,7 +76,7 @@ function PaginationPrevious({
       className={cn("gap-1 px-2.5 sm:pl-2.5", className)}
       {...props}
     >
-      <ChevronLeftIcon />
+      <IconChevronLeft />
       <span className="hidden sm:block">Previous</span>
     </PaginationLink>
   );
@@ -94,7 +94,7 @@ function PaginationNext({
       {...props}
     >
       <span className="hidden sm:block">Next</span>
-      <ChevronRightIcon />
+      <IconChevronRight />
     </PaginationLink>
   );
 }
@@ -110,7 +110,7 @@ function PaginationEllipsis({
       className={cn("flex size-9 items-center justify-center", className)}
       {...props}
     >
-      <MoreHorizontalIcon className="size-4" />
+      <IconDots className="size-4" />
       <span className="sr-only">More pages</span>
     </span>
   );
---
+++ lib/icons.ts
@@ -0,0 +1,98 @@
+import type { IconType } from "react-icons";
+import {
+  IoAdd,
+  IoAnalyticsOutline,
+  IoArrowForward,
+  IoBarChartOutline,
+  IoBriefcaseOutline,
+  IoCalendarOutline,
+  IoCardOutline,
+  IoCheckmark,
+  IoChevronBack,
+  IoChevronDown,
+  IoChevronForward,
+  IoChevronUp,
+  IoClose,
+  IoCloudUploadOutline,
+  IoDocumentOutline,
+  IoDownloadOutline,
+  IoEllipsisHorizontal,
+  IoEyeOffOutline,
+  IoEyeOutline,
+  IoFilterOutline,
+  IoGridOutline,
+  IoHelpCircleOutline,
+  IoListOutline,
+  IoLockClosedOutline,
+  IoLogOutOutline,
+  IoLogoGoogle,
+  IoLogoWindows,
+  IoMailOutline,
+  IoMenuOutline,
+  IoNotificationsOutline,
+  IoOptionsOutline,
+  IoPeopleOutline,
+  IoPersonCircleOutline,
+  IoPrintOutline,
+  IoPulseOutline,
+  IoReorderThreeOutline,
+  IoSearchOutline,
+  IoServerOutline,
+  IoSettingsOutline,
+  IoShieldCheckmarkOutline,
+  IoSpeedometerOutline,
+  IoSwapVerticalOutline,
+  IoTrashOutline,
+  IoTrendingDownOutline,
+  IoTrendingUpOutline,
+} from "react-icons/io5";
+
+export type Icon = IconType;
+
+export const IconActivity = IoPulseOutline;
+export const IconAdjustmentsHorizontal = IoOptionsOutline;
+export const IconArrowRight = IoArrowForward;
+export const IconArrowsSort = IoSwapVerticalOutline;
+export const IconBell = IoNotificationsOutline;
+export const IconBrandGoogleFilled = IoLogoGoogle;
+export const IconBrandWindows = IoLogoWindows;
+export const IconBriefcase = IoBriefcaseOutline;
+export const IconCalendarTime = IoCalendarOutline;
+export const IconChartBar = IoBarChartOutline;
+export const IconCheck = IoCheckmark;
+export const IconChevronDown = IoChevronDown;
+export const IconChevronLeft = IoChevronBack;
+export const IconChevronRight = IoChevronForward;
+export const IconChevronUp = IoChevronUp;
+export const IconCreditCard = IoCardOutline;
+export const IconDashboard = IoSpeedometerOutline;
+export const IconDatabase = IoServerOutline;
+export const IconDots = IoEllipsisHorizontal;
+export const IconEye = IoEyeOutline;
+export const IconEyeOff = IoEyeOffOutline;
+export const IconFile = IoDocumentOutline;
+export const IconFileSpreadsheet = IoGridOutline;
+export const IconFilter = IoFilterOutline;
+export const IconHelpCircle = IoHelpCircleOutline;
+export const IconLayoutSidebarLeftExpand = IoMenuOutline;
+export const IconListDetails = IoListOutline;
+export const IconLock = IoLockClosedOutline;
+export const IconLogout = IoLogOutOutline;
+export const IconMail = IoMailOutline;
+export const IconNotification = IoNotificationsOutline;
+export const IconPlus = IoAdd;
+export const IconPrinter = IoPrintOutline;
+export const IconReportAnalytics = IoAnalyticsOutline;
+export const IconSidebarToggle = IoReorderThreeOutline;
+export const IconSearch = IoSearchOutline;
+export const IconSettings = IoSettingsOutline;
+export const IconShieldCheck = IoShieldCheckmarkOutline;
+export const IconTableExport = IoDownloadOutline;
+export const IconTrash = IoTrashOutline;
+export const IconTrendingDown = IoTrendingDownOutline;
+export const IconTrendingUp = IoTrendingUpOutline;
+export const IconUpload = IoCloudUploadOutline;
+export const IconUserCircle = IoPersonCircleOutline;
+export const IconUsers = IoPeopleOutline;
+export const IconUsersGroup = IoPeopleOutline;
+export const IconX = IoClose;
